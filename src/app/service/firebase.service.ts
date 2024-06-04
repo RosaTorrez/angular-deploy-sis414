9299f83c67cfacafce0e94f2d7dae32892409e38
Rosa Torrez Quispe <rosatorrezquispe23@gmail.com>
2024-06-04 13:35:52 -0400

    unp

---
+++ src/app/service/firebase.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FirebaseService {
+urlget:string=""
+
+  constructor() { }
+  getData():Observable<any>{
+      
+  }
+}
+//
